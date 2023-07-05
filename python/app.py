7b919e50a7e50506afe5618f917699eafdd0a572
MANIVSMCA <manimv1268@gmail.com>
2023-07-05 17:20:35 +0530

    create the dockerfile for python

--- python/app.py
+++ python/app.py
@@ -1,19 +1,12 @@
-from flask import Flask,render_template
-#import socket
-import os
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    #try:
- #       host_name = socket.gethostname()
-  #      host_ip = socket.gethostbyname(host_name)
-     #   return render_template('index.html')
-        #, hostname=host_name, ip=host_ip)
-    #except:
-        return render_template('index.html')
-
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0')
+
+# Why you should run it at 0.0.0.0
+
